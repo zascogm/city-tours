674e7e870589ce3c70bbfc32e893a3367e825e84
zascogm <zascogm@gmail.com>
2020-05-27 17:12:40 +1000

    Adding Tour component logic

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,10 +3,30 @@ import './tour.scss'
 
 export default class Tour extends Component {
     render() {
+        // console.log(this.props);
+        const { id, city, img, name, info } = this.props.tour;
+        const { removeTour } = this.props;
+
         return (
-            <div>
-                Hello from Tour
-            </div>
-        )
+            <articule className="tour">
+                <div className="img-container">
+                    <img src={img} alt="tour" />
+                    <span className="close-btn">
+                        <i className="fas fa-window-close" />
+                    </span>
+                </div>
+                <div className="tour-info">
+                    <h3>{city}</h3>
+                    <h4>{name}</h4>
+                    <h5>
+                        info{" "}
+                        <span>
+                            <i className="fas fa-caret-square-down"/>
+                        </span>
+                    </h5>
+                    <p>{info}</p>
+                </div>
+            </articule>
+        );
     }
 }
