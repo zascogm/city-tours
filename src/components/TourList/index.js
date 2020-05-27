6084161d77f883fac5f3b4662708fdec4cc127a8
zascogm <zascogm@gmail.com>
2020-05-27 16:53:06 +1000

    Adding TourList component logic

--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import Tour from '../Tour'
+import './tourlist.scss'
+import { tourData } from '../../tourData'
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData
+    };
+
+    removeTour = id => {
+        console.log(id);
+    };
+
     render() {
+        // console.log(this.state.tours);
+        const { tours } = this.state;
+
         return (
-            <div>
-                Hello from TourList
-                <Tour />
-            </div>
-        )
+            <section className="tourlist">
+                {
+                    tours.map(
+                        tour => (
+                            <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
+                        )
+                    )
+                }
+            </section>
+        );
     }
 }
